c3912977bd0afabe3cff29630139e9bd40d57bf5
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-17 09:52:29 -0800

    write third test but without accompanying method in the object file

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -7,7 +7,7 @@ namespace RepeatCounterFunction.Objects
     public class RepeatCounterTest
     {
         [Fact]
-        public void CountRepeats_ReturnInputs_AllInputs()
+        public void CountRepeats_ReturnInputs_allInputs()
         {
             RepeatCounter testRepeatCounter = new RepeatCounter("this is a phrase", "pickle");
             List<string> expected = new List<string>{"this is a phrase", "pickle"};
@@ -19,9 +19,17 @@ namespace RepeatCounterFunction.Objects
         public void CountRepeats_ReturnTrueIfMatch_true()
         {
             RepeatCounter testRepeatCounter = new RepeatCounter("I like rowing", "rowing");
-            // string expected = new string("true");
             bool output = testRepeatCounter.TrueFalse();
             Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void CountRepeats_ReturnCount_numberOfMatches()
+        {
+            RepeatCounter testRepeatCounter = new RepeatCounter("Every dog is cute but my favorite kind of dog is a happy dog", "dog");
+            int expected = 3;
+            int output = testRepeatCounter.CountRepeats();
+            Assert.Equal(expected, output);
         }
     }
 }
