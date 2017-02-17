59552cb8c30a9e2f0ee1808bbcb417cf38cae362
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-17 08:51:21 -0800

    write first test, don't start object file yet

---
+++ Tests/WordCounterTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+
+namespace RepeatCounterFunction.Objects
+{
+  public class RepeatCounterTest
+  {
+    [Fact]
+    public void CountRepeats_ReturnInputs_AllInputs()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter("this is a phrase", "pickle");
+      List<string> expected = new List<string>{"this is a phrase", "pickle"};
+      List<string> output = testRepeatCounter.GetInputs();
+      Assert.Equal(expected, output);
+    }
+  }
+}
